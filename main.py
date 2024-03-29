00309e11f7c30d98bd8448ccf9601efbe0d6780c
just_pr()grammer <avazbekabdurafikov21@gmail.com>
2024-03-29 21:39:49 +0500

    Description, images and url

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import streamlit as st
-
-st.set_page_config(layout="wide")
+import pandas
+# st.set_page_config(layout="wide")
 col1, col2 = st.columns(2)
 
 with col1:
@@ -13,3 +13,26 @@ with col2:
     Pyhton with Ardit Sulce my teacher for Python!
     """
     st.info(content)
+
+st.write("Below you can find some of apps i have built in Python. Feel free contact me")
+
+col3, empty_col, col4 = st.columns([1.5, 0.5, 1.5])
+
+df = pandas.read_csv("data.csv", sep=";")
+
+
+with col3:
+    for index, row in df[:10].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write(f'[Source code]({row["url"]})')
+
+
+with col4:
+    for index, row in df[10:].iterrows():
+        st.header(row["title"])
+        st.write(row["description"])
+        st.image(f"images/{row['image']}")
+        st.write(f'[Source code]({row["url"]})')
+
